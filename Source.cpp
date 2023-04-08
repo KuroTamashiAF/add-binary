fb0efd2f485b122ddde5b01cb3e09a236ef4285b
Fedorov <chugun777akk@mail.ru>
2023-04-08 11:40:37 +0700

    add fuction twoToTen

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,27 @@
 //Given two binary strings a and b, return their sum as a binary string.
 #include <iostream>
+#include <string>
+#include <math.h>
 using namespace std;
 class Solution {
 public:
     string addBinary(string a, string b) {
+        int a1 = twoToTen(a);
+        int a2 = twoToTen(b);
+        int res = a1 + a2;
 
+        return "";
     }
+    int twoToTen(string a)
+    {
+        int res = NULL;
+        for (int i = 0; i < a.length(); i++)
+        {
+            res += ((int)a[i]-48) * pow(2, a.length() - 1-i);
+        }
+        return res;
+    }
+
 };
 
 
