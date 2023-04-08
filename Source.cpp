99908badd750faa9f7e27691880d9c277822ac06
Fedorov <chugun777akk@mail.ru>
2023-04-08 11:00:31 +0700

    prepared the project

---
+++ Source.cpp
@@ -0,0 +1,28 @@
+//Given two binary strings a and b, return their sum as a binary string.
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    string addBinary(string a, string b) {
+
+    }
+};
+
+
+int main()
+{
+    Solution s;
+    string input1 = "1010";
+    string input2 = "1011";
+    cout << s.addBinary(input1, input2) << endl;
+
+
+    return 0;
+}
+
+
+
+
+
+
+
